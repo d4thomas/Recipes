57ac21273c67db25d21999829f4f38c1b29590e3
d4thomas <152221911+d4thomas@users.noreply.github.com>
2025-09-30 12:35:37 -0400

    chore: add remaining services and corresponding tests

--- backend/src/__tests__/recipes.test.js
+++ backend/src/__tests__/recipes.test.js
@@ -1,6 +1,14 @@
 import mongoose from 'mongoose'
-import { describe, expect, test } from '@jest/globals'
-import { createRecipe } from '../services/recipes.js'
+import { describe, expect, test, beforeEach } from '@jest/globals'
+import {
+  createRecipe,
+  listAllRecipes,
+  listRecipesByAuthor,
+  listRecipesByTag,
+  getRecipeById,
+  updateRecipe,
+  deleteRecipe,
+} from '../services/recipes.js'
 import { Recipe } from '../db/models/recipe.js'
 
 describe('creating recipes', () => {
@@ -39,5 +47,119 @@ describe('creating recipes', () => {
     }
     const createdRecipe = await createRecipe(recipe)
     expect(createdRecipe._id).toBeInstanceOf(mongoose.Types.ObjectId)
+  })
+})
+
+// Define sample recipes
+const sampleRecipes = [
+  { title: 'Recipe #1', author: 'Daniel Thomas', tags: ['food'] },
+  { title: 'Recipe #2', author: 'Daniel Thomas', tags: ['food'] },
+  {
+    title: 'Recipe #3',
+    author: 'Daniel Thomas',
+    tags: ['more food', 'food'],
+  },
+  { title: 'Recipe #4' },
+]
+
+let createdSampleRecipes = []
+beforeEach(async () => {
+  await Recipe.deleteMany({})
+  createdSampleRecipes = []
+  for (const recipe of sampleRecipes) {
+    const createdRecipe = new Recipe(recipe)
+    createdSampleRecipes.push(await createdRecipe.save())
+  }
+})
+
+describe('listing recipes', () => {
+  test('should return all recipes', async () => {
+    const recipes = await listAllRecipes()
+    expect(recipes.length).toEqual(createdSampleRecipes.length)
+  })
+  test('should return recipes sorted by creation date descending by default', async () => {
+    const recipes = await listAllRecipes()
+    const sortedSampleRecipes = createdSampleRecipes.sort(
+      (a, b) => b.createdAt - a.createdAt,
+    )
+    expect(recipes.map((recipe) => recipe.createdAt)).toEqual(
+      sortedSampleRecipes.map((recipe) => recipe.createdAt),
+    )
+  })
+  test('should take into account provided sorting options', async () => {
+    const recipes = await listAllRecipes({
+      sortBy: 'updatedAt',
+      sortOrder: 'ascending',
+    })
+    const sortedSampleRecipes = createdSampleRecipes.sort(
+      (a, b) => a.updatedAt - b.updatedAt,
+    )
+    expect(recipes.map((recipe) => recipe.updatedAt)).toEqual(
+      sortedSampleRecipes.map((recipe) => recipe.updatedAt),
+    )
+  })
+  test('should be able to filter recipes by author', async () => {
+    const recipes = await listRecipesByAuthor('Daniel Thomas')
+    expect(recipes.length).toBe(3)
+  })
+  test('should be able to filter recipes by tag', async () => {
+    const recipes = await listRecipesByTag('more food')
+    expect(recipes.length).toBe(1)
+  })
+})
+
+describe('getting a recipe', () => {
+  test('should return the full recipe', async () => {
+    const recipe = await getRecipeById(createdSampleRecipes[0]._id)
+    expect(recipe.toObject()).toEqual(createdSampleRecipes[0].toObject())
+  })
+  test('should fail if the id does not exist', async () => {
+    const recipe = await getRecipeById('000000000000000000000000')
+    expect(recipe).toEqual(null)
+  })
+})
+
+describe('updating recipes', () => {
+  test('should update the specified property', async () => {
+    await updateRecipe(createdSampleRecipes[0]._id, {
+      author: 'Test Author',
+    })
+    const updatedRecipe = await Recipe.findById(createdSampleRecipes[0]._id)
+    expect(updatedRecipe.author).toEqual('Test Author')
+  })
+  test('should not update other properties', async () => {
+    await updateRecipe(createdSampleRecipes[0]._id, {
+      author: 'Test Author',
+    })
+    const updatedRecipe = await Recipe.findById(createdSampleRecipes[0]._id)
+    expect(updatedRecipe.title).toEqual('Recipe #1')
+  })
+  test('should update the updatedAt timestamp', async () => {
+    await updateRecipe(createdSampleRecipes[0]._id, {
+      author: 'Test Author',
+    })
+    const updatedRecipe = await Recipe.findById(createdSampleRecipes[0]._id)
+    expect(updatedRecipe.updatedAt.getTime()).toBeGreaterThan(
+      createdSampleRecipes[0].updatedAt.getTime(),
+    )
+  })
+  test('should fail if the id does not exist', async () => {
+    const recipe = await updateRecipe('000000000000000000000000', {
+      author: 'Test Author',
+    })
+    expect(recipe).toEqual(null)
+  })
+})
+
+describe('deleting recipes', () => {
+  test('should remove the recipe from the database', async () => {
+    const result = await deleteRecipe(createdSampleRecipes[0]._id)
+    expect(result.deletedCount).toEqual(1)
+    const deletedRecipe = await Recipe.findById(createdSampleRecipes[0]._id)
+    expect(deletedRecipe).toEqual(null)
+  })
+  test('should fail if the id does not exist', async () => {
+    const result = await deleteRecipe('000000000000000000000000')
+    expect(result.deletedCount).toEqual(0)
   })
 })
--- backend/src/services/recipes.js
+++ backend/src/services/recipes.js
@@ -5,3 +5,47 @@ export async function createRecipe({ title, author, contents, tags }) {
   const recipe = new Recipe({ title, author, contents, tags })
   return await recipe.save()
 }
+
+// List recipes
+async function listRecipes(
+  query = {},
+  { sortBy = 'createdAt', sortOrder = 'descending' } = {},
+) {
+  return await Recipe.find(query).sort({ [sortBy]: sortOrder })
+}
+
+export async function listAllRecipes(options) {
+  return await listRecipes({}, options)
+}
+
+// List recipes by author
+export async function listRecipesByAuthor(author, options) {
+  return await listRecipes({ author }, options)
+}
+
+// List recipes by tag
+export async function listRecipesByTag(tags, options) {
+  return await listRecipes({ tags }, options)
+}
+
+// Get recipe by recipe ID
+export async function getRecipeById(recipeId) {
+  return await Recipe.findById(recipeId)
+}
+
+// Update recipe
+export async function updateRecipe(
+  recipeId,
+  { title, author, contents, tags },
+) {
+  return await Recipe.findOneAndUpdate(
+    { _id: recipeId },
+    { $set: { title, author, contents, tags } },
+    { new: true },
+  )
+}
+
+// Delete recipe
+export async function deleteRecipe(recipeId) {
+  return await Recipe.deleteOne({ _id: recipeId })
+}
