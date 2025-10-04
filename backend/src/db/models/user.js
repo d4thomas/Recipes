f067ba39849d54f56fcee9f08fd150daef563c51
d4thomas <152221911+d4thomas@users.noreply.github.com>
2025-10-04 14:13:34 -0400

    chore: update comments

--- backend/src/db/models/user.js
+++ backend/src/db/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
 
+// Define user schema
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
