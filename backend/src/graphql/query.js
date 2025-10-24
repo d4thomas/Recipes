5403cef10f99d1c1ebf6069f657ff68027cff10a
d4thomas <152221911+d4thomas@users.noreply.github.com>
2025-10-24 13:35:40 -0400

    chore: add options to graphql

--- backend/src/graphql/query.js
+++ backend/src/graphql/query.js
@@ -6,12 +6,16 @@ import {
 } from '../services/recipes.js'
 
 export const querySchema = `#graphql
+  input RecipesOptions {
+    sortBy: String,
+    sortOrder: String
+  }
   type Query {
       test: String
-      recipes: [Recipe!]!
-      recipesByAuthor(username: String!): [Recipe!]!
-      recipesByTag(tag: String!): [Recipe!]!
-      recipeById(id: ID!): Recipe
+      recipes(options: RecipesOptions): [Recipe!]!
+      recipesByAuthor(username: String!, options: RecipesOptions): [Recipe!]!
+      recipesByTag(tag: String!, options: RecipesOptions): [Recipe!]!
+      recipeById(id: ID!, options: RecipesOptions): Recipe
   }
 `
 export const queryResolver = {
@@ -19,14 +23,14 @@ export const queryResolver = {
     test: () => {
       return 'Hello World from GraphQL!'
     },
-    recipes: async () => {
-      return await listAllRecipes()
+    recipes: async (parent, { options }) => {
+      return await listAllRecipes(options)
     },
-    recipesByAuthor: async (parent, { username }) => {
-      return await listRecipesByAuthor(username)
+    recipesByAuthor: async (parent, { username, options }) => {
+      return await listRecipesByAuthor(username, options)
     },
-    recipesByTag: async (parent, { tag }) => {
-      return await listRecipesByTag(tag)
+    recipesByTag: async (parent, { tag, options }) => {
+      return await listRecipesByTag(tag, options)
     },
     recipeById: async (parent, { id }) => {
       return await getRecipeById(id)
